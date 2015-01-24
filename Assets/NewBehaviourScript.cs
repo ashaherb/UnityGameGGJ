c53fa4e0bda4ad8fec76a9b0a4a37b68f00eb7b9
Hussein Adel <sona_spider@yahoo.com>
2015-01-24 23:00:55 +0200

    buttons Commands

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Play : MonoBehaviour {
-	
+public class NewBehaviourScript : MonoBehaviour {
+
 	void OnGUI() {
 		if(GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 1, 200, 30), "Demooo"))
 			Application.LoadLevel(1);
-		
+
 	}
 }
